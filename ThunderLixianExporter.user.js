66f79f5f0235b671293a81bf2b9771e03e05106d
liprais <ipfoox@gmail.com>
2012-11-11 19:46:29 +0800

    Update ThunderLixianExporter.user.js

--- ThunderLixianExporter.user.js
+++ ThunderLixianExporter.user.js
@@ -13,8 +13,8 @@
 var script = document.createElement('script');
 script.id = "TLE_script";
 if (location.host == "dynamic.cloud.vip.xunlei.com") {
-  script.src = "https://raw.github.com/binux/ThunderLixianExporter/master/ThunderLixianExporter.js";
+  script.src = "https://raw.github.com/liprais/ThunderLixianExporter/master/ThunderLixianExporter.js";
 } else {
-  script.src = "https://raw.github.com/binux/ThunderLixianExporter/master/vod_html5.js";
+  script.src = "https://raw.github.com/liprais/ThunderLixianExporter/master/vod_html5.js";
 }
 document.body.appendChild(script);
